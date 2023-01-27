1887b3d73db14015bbeeafe7d8d9652154056832
Bogdan Pinchuk <bogdan.pinchuk@rubyplay.com>
2023-01-27 15:10:18 +0200

    added composite code

---
+++ composite/src/main/java/refactoring_guru/Main.java
@@ -0,0 +1,28 @@
+package refactoring_guru;
+
+import editor.ImageEditor;
+import java.awt.Color;
+import shapes.Circle;
+import shapes.CompoundShape;
+import shapes.Dot;
+import shapes.Rectangle;
+
+public class Main {
+	public static void main(String[] args) {
+		ImageEditor editor = new ImageEditor();
+		editor.loadShapes(
+				new Circle(10, 10, 10, Color.BLUE),
+				new CompoundShape(
+						new Circle(110, 110, 50, Color.RED),
+						new Dot(160, 160, Color.RED)),
+				new CompoundShape(
+						new Rectangle(250, 250, 100, 100, Color.GREEN),
+						new Dot(240, 240, Color.GREEN),
+						new Dot(240, 360, Color.GREEN),
+						new Dot(360, 360, Color.GREEN),
+						new Dot(360, 240, Color.GREEN))
+		);
+
+		System.out.println("\nFinished!");
+	}
+}
