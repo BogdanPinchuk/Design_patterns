5e92344194bd3b1659f531f52f1220bc5bea4466
Bogdan Pinchuk <bogdan.pinchuk@rubyplay.com>
2023-05-23 04:08:33 +0300

    added observer code

---
+++ observer/src/main/java/listeners/EmailNotificationListener.java
@@ -0,0 +1,17 @@
+package listeners;
+
+import java.io.File;
+
+public class EmailNotificationListener implements EventListener {
+	private final String email;
+
+	public EmailNotificationListener(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public void update(String eventType, File file) {
+		System.out.println("Email to " + email + ": Someone has performed " + eventType +
+				" operation with the following file: " + file.getName());
+	}
+}
