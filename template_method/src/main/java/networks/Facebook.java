026ce99e1b09b6681d04cb76e4660bba972f5d37
Bogdan Pinchuk <bogdan.pinchuk@rubyplay.com>
2023-05-24 06:45:34 +0300

    added template_method code

---
+++ template_method/src/main/java/networks/Facebook.java
@@ -0,0 +1,50 @@
+package networks;
+
+public class Facebook extends Network {
+    public Facebook(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    @Override
+    public boolean logIn(String userName, String password) {
+        System.out.println("\nChecking user's parameters");
+        System.out.println("Name: " + this.userName);
+        System.out.print("Password: ");
+        for (int i = 0; i < this.password.length(); i++) {
+            System.out.print("*");
+        }
+        simulateNetworkLatency();
+        System.out.println("\n\nLogIn success on Facebook");
+        return true;
+    }
+
+    private void simulateNetworkLatency() {
+        try {
+            int i = 0;
+            System.out.println();
+            while (i < 10) {
+                System.out.print(".");
+                Thread.sleep(500);
+                i++;
+            }
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public boolean sendData(byte[] data) {
+        boolean messagePosted = data != null;
+        if (messagePosted) {
+            System.out.println("Message: '" + new String(data) + "' was posted on Facebook");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    void logOut() {
+        System.out.println("User: '" + userName + "' was logged out from Facebook");
+    }
+}
